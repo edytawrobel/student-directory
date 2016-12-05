fa4a3f13f012aadbd6a7a2adbe34a759e293410a
Edyta Wrobel <edyta.wrobel.willdoit@gmail.com>
2016-12-05 14:02:31 +0000

    Array-iteration to print the list of students

--- directory.rb
+++ directory.rb
@@ -1,19 +1,25 @@
-#first we print the list of students
-
-student_count = 11
+#let's put all students into an array
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
 
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
-#we can split the text in between a few lines with "\n"
-print "Overall, we have\n#{student_count}\ngreat students"
+#puts students[0..10]
+#array iteration to print the list of all students
+students.each do |student|
+  puts student
+end
+
+#using the array's count() method to count students
+print "Overall, we have #{students.count} great students"
